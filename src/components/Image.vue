fdb5ec572168a1a5ef853ec55d3365e8278162d3
bob <swagcheeseyt@gmail.com>
2023-10-02 16:10:43 -0700

    initial gallery

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -1,11 +1,20 @@
 <script setup>
   defineProps(['image']);
+  defineEmits(['makeBigger']);
+  
+  function makeBigger() {
+      // make the image fill most of the screen with an x in the top right corner
+  }
 </script>
 
 <template>
-
+    <img :src="image"/>
 </template>
 
 <style scoped>
-
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
 </style>
