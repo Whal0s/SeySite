45c98cbdbd08abfb7c3ab3eaee89c4542a03c477
bob <swagcheeseyt@gmail.com>
2023-10-02 17:25:34 -0700

    gallery

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,32 +1,31 @@
 <script setup>
+  import GalleryColumn from "./GalleryColumn.vue";
+  import {onMounted} from "vue";
+  defineProps(['images']);
+  
+  onMounted(() => {
+  
+  })
 </script>
 
 <template>
   <div id="base">
     <div class="frame">
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dolor i
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse est facere impedit magni nobis sequi.
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse est facere impedit magni nobis sequi.
-      </div>
+        <div class="column" v-for="imageArr in images">
+            <GalleryColumn :images="imageArr"/>
+        </div>
     </div>
   </div>
 </template>
 
 
 <style scoped>
-
-  #base{
+  #base {
     background-color: #535bf2;
     width: 90%;
     margin: 5%;
     .frame {
       display: grid;
-      grid-template-columns: 1fr 1fr 1fr;
     }
   }
 </style>
