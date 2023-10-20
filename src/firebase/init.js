ee47e2b82b8ce1ab284e066299b313d23c3e9a7d
Whal0s <jordanlo8223@gmail.com>
2023-10-19 21:41:58 -0700

    Hi bobby

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -23,21 +23,21 @@ const storage = getStorage();
 const database = getFirestore();
 
 //Authentication
-const provider = new OAuthProvider('microsoft.com');
-const auth = getAuth();
-signInWithPopup(auth, provider)
-    .then((result) => {
-        // User is signed in.
-        // IdP data available in result.additionalUserInfo.profile.
+// const provider = new OAuthProvider('microsoft.com');
+// const auth = getAuth();
+// signInWithPopup(auth, provider)
+//     .then((result) => {
+//         // User is signed in.
+//         // IdP data available in result.additionalUserInfo.profile.
+//
+//         // Get the OAuth access token and ID Token
+//         const credential = OAuthProvider.credentialFromResult(result);
+//         const accessToken = credential.accessToken;
+//         const idToken = credential.idToken;
+//     })
+//     .catch((error) => {
+//         // Handle error.
+//     });
 
-        // Get the OAuth access token and ID Token
-        const credential = OAuthProvider.credentialFromResult(result);
-        const accessToken = credential.accessToken;
-        const idToken = credential.idToken;
-    })
-    .catch((error) => {
-        // Handle error.
-    });
-
-export { database, storage, signInWithPopup }
+export { database, storage}
 
--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import './styles.scss'
 import App from './App.vue'
 
-createApp(App).mount('#app')
-
+const useVfm = createVfm();
+createApp(App).use(useVfm).mount('#app')
